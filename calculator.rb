f60fac0803247c7d42fe48cce51c062c2c43c87e
Fu <fu.goto@gmail.com>
2020-04-26 17:42:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,12 @@
 # Add your variables here
-first_number = "hi"
+first_number = 1
+
+second_number = 1
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
